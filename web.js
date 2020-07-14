8b0a0d40379464bc699faedc1160c168629f1c5e
emalgeorge1234@gmail.com <emalgeorge1234@gmail.com>
2020-07-14 22:58:01 +0530

    final

---
+++ web.js
@@ -0,0 +1,45 @@
+  $(document).ready(function(){
+    $("#cls").on({
+        mouseenter: function(){
+            $(this).css("background-color", "#C0C0C0");
+        },  
+       
+        mouseleave: function(){
+            $(this).css("background-color", "white");
+        },
+       
+        dblclick: function(){
+            $(this).css("background-color", "yellow");
+        }
+    });
+});
+$(document).ready(function(){
+    $("#ash").on({
+        mouseenter: function(){
+            $(this).css("background-color", "#C0C0C0");
+        },  
+       
+        mouseleave: function(){
+            $(this).css("background-color", "white");
+        },
+       
+        dblclick: function(){
+            $(this).css("background-color", "yellow");
+        }
+    });
+});
+$(document).ready(function(){
+    $("#diya").on({
+        mouseenter: function(){
+            $(this).css("background-color", "#C0C0C0");
+        },  
+       
+        mouseleave: function(){
+            $(this).css("background-color", "white");
+        },
+       
+        dblclick: function(){
+            $(this).css("background-color", "white");
+        }
+    });
+});
